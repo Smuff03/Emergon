db6549c7473d09e0454c5358eb84f5259e5c1ef9
Samarth <samarth8902@gmail.com>
2022-08-17 22:02:09 +0530

    Emergon app

--- app/src/main/java/com/example/emergon/MainActivity.java
+++ app/src/main/java/com/example/emergon/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
+//i am samarth
 public class MainActivity extends AppCompatActivity {
 
     @Override
