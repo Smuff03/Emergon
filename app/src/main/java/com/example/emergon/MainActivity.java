7bd3ee8496cae831d0fbe9e3fda4d1e38d3962e3
JayeshX <111035131+JayeshX@users.noreply.github.com>
2022-09-10 10:48:45 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/emergon/MainActivity.java
+++ app/src/main/java/com/example/emergon/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.emergon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -14,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+        String checkbox = preferences.getString("b","");
+        String log = preferences.getString("login","");
+        if(checkbox.equals("true")){
+            if(log.equals("patient")){
+            Intent intent = new Intent(MainActivity.this,bio.class);
+            startActivity(intent);}
+            else if(log.equals("hospital")){
+                Intent intent = new Intent(MainActivity.this,hlogin.class);
+                startActivity(intent);
+            }
+        }else if(checkbox.equals("false")){
+            Toast.makeText(this,"please sign in",Toast.LENGTH_SHORT).show();
+        }
+
+
     }
     public void openActivity(View view) {
         Toast.makeText(this, "opening Patient login", Toast.LENGTH_SHORT).show();
