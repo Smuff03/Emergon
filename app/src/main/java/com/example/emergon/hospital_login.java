e06333bf4352a5ad3b9169569f914b2f2f97925b
JayeshX <111035131+JayeshX@users.noreply.github.com>
2022-09-10 10:49:28 +0530

    Update hospital_login.java

--- app/src/main/java/com/example/emergon/hospital_login.java
+++ app/src/main/java/com/example/emergon/hospital_login.java
@@ -3,16 +3,72 @@ package com.example.emergon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class hospital_login extends AppCompatActivity {
-
+    private EditText hname,hreg,pass;
+    private CheckBox cb;
+    private Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_login);
+        hname= findViewById(R.id.editTextTextPersonName);
+        pass = findViewById(R.id.editTextTextPassword);
+        hreg = findViewById(R.id.editTextTextPersonName2);
+        cb = findViewById(R.id.cb3);
+        b = findViewById(R.id.button4);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String hname1 = hname.getText().toString();
+                String ps = pass.getText().toString();
+                String hr = hreg.getText().toString();
+
+                if(hname1.isEmpty() || ps.isEmpty()|| hr.isEmpty()){
+                    Toast.makeText(hospital_login.this,"please enter all data",Toast.LENGTH_SHORT).show();
+                }else{
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("hospital_name",hname1);
+                    editor.putString("hospital_password",ps);
+                    editor.putString("hospital_registration",hr);
+                    editor.putString("login","hospital");
+                    editor.apply();
+                    Intent intent = new Intent(hospital_login.this, hlogin.class);
+                    startActivity(intent);
+
+                }
+
+            }
+        });
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("b","true");
+                    editor.apply();
+                    Toast.makeText(hospital_login.this,"Checked",Toast.LENGTH_SHORT).show();
+
+                }else if(!compoundButton.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("b","false");
+                    editor.apply();
+                    Toast.makeText(hospital_login.this,"UnChecked",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
     public void new_hospital(View view) {
