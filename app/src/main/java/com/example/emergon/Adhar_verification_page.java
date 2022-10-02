b4177b20da942c2d7053ef442f66819589eccfd0
Tejas Mayekar <tejasmayekar08@gmail.com>
2022-10-02 22:22:14 +0530

    Added Scanner page and adhar page with input field adding scanner functionality is remaining

---
+++ app/src/main/java/com/example/emergon/Adhar_verification_page.java
@@ -0,0 +1,14 @@
+package com.example.emergon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Adhar_verification_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adhar_verification_page);
+    }
+}
--- app/src/main/java/com/example/emergon/hlogin.java
+++ app/src/main/java/com/example/emergon/hlogin.java
@@ -3,8 +3,12 @@ package com.example.emergon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class hlogin extends AppCompatActivity {
     private TextView tv;
@@ -15,5 +19,27 @@ public class hlogin extends AppCompatActivity {
         tv = findViewById(R.id.textView3);
 
         tv.setText(String.format("welcome to hospital "));
+        Button Scanner = findViewById(R.id.Scanner);
+        Scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Scanneropen = new Intent(hlogin.this,Scanner_page.class);
+                startActivity(Scanneropen);
+                Toast.makeText(getApplicationContext(), "Opening Scanner",Toast.LENGTH_LONG).show();
+            }
+        });
+        //adhar ke button ko define kiya aur uska activity bhi
+        Button Addhar_btn = findViewById(R.id.Adhhar);
+        Addhar_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //iske wajah se object create hota hai
+                Intent Addhar_page_open = new Intent(hlogin.this,Adhar_verification_page.class);
+                //niche wale se page ko jata hai
+                startActivity(Addhar_page_open);
+                //ye niche wala message deta hai
+                Toast.makeText(getApplicationContext(),"Opening Adhar verification page",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
