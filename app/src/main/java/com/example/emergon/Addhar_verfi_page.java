8b283c6951fcd6b154cf92e41d04b1d4e4d9ce67
Tejas Mayekar <tejasmayekar08@gmail.com>
2022-10-04 00:31:29 +0530

    Layout corrected new pages google-services.json added scanner function remaining

---
+++ app/src/main/java/com/example/emergon/Addhar_verfi_page.java
@@ -0,0 +1,14 @@
+package com.example.emergon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addhar_verfi_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addhar_verfi_page);
+    }
+}
--- app/src/main/java/com/example/emergon/hlogin.java
+++ app/src/main/java/com/example/emergon/hlogin.java
@@ -3,7 +3,10 @@ package com.example.emergon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class hlogin extends AppCompatActivity {
@@ -12,8 +15,22 @@ public class hlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hlogin);
-        tv = findViewById(R.id.textView3);
 
-        tv.setText(String.format("welcome to hospital "));
+        Button Scanner_btn = findViewById(R.id.Scanner_btn);
+        Scanner_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goTOScannerPage = new Intent(hlogin.this,Scanner_page.class);
+                startActivity(goTOScannerPage);
+            }
+        });
+        Button addhar_btn = findViewById(R.id.Adhhar_btn);
+        addhar_btn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddharVerifiPage = new Intent(hlogin.this,Addhar_verfi_page.class);
+                startActivity(goToAddharVerifiPage);
+            }
+        }));
     }
 }
