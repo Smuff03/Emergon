3785d07835c181848d6ae64ec8b66ed761ea8894
JayeshX <jayeshkirtane218@gmail.com>
2022-10-04 18:47:28 +0530

    new

--- app/src/main/java/com/example/emergon/hlogin.java
+++ app/src/main/java/com/example/emergon/hlogin.java
@@ -1,19 +1,76 @@
 
 package com.example.emergon;
+import android.content.DialogInterface;
+import android.content.Intent;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class hlogin extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class hlogin extends AppCompatActivity implements View.OnClickListener{
     private TextView tv;
+    private Button sc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hlogin);
         tv = findViewById(R.id.textView3);
-
         tv.setText(String.format("welcome to hospital "));
+        sc = findViewById(R.id.qrbut);
+        sc.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        scanCode();
+    }
+
+    private void scanCode(){
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scanning code");
+        integrator.initiateScan();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result  = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if(result.getContents() !=null){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                String s = result.getContents();
+                Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+                builder.setMessage(result.getContents());
+                builder.setTitle("scanning result");
+                builder.setPositiveButton("scan Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        scanCode();
+                    }
+                }).setNegativeButton("finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            else{
+                Toast.makeText(this,"No Results",Toast.LENGTH_LONG).show();
+            }
+        }else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+
     }
 }
--- app/src/main/java/com/example/emergon/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/emergon/ui/home/HomeFragment.java
@@ -1,20 +1,29 @@
 package com.example.emergon.ui.home;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.emergon.R;
 import com.example.emergon.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    EditText pt_phone;
+    Button homebut;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +32,24 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        Activity activity = getActivity();
+        pt_phone = root.findViewById(R.id.pat_phoneno);
+        homebut = root.findViewById(R.id.homeconfirm);
+        homebut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = String.valueOf(pt_phone);
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(activity, "Enter Valid Phone No.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    SharedPreferences preferences = activity.getSharedPreferences("checkbox",activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("pat_phone",phone);
+                    editor.apply();
+                }
+            }
+        });
 
         return root;
     }
