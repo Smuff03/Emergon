b97bddb71eb2ff3f038f4a7baecbfe358d803d86
Tejas Mayekar <tejasmayekar08@gmail.com>
2022-10-05 17:06:29 +0530

    qr image

--- app/src/main/java/com/example/emergon/hlogin.java
+++ app/src/main/java/com/example/emergon/hlogin.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class hlogin extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class hlogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hlogin);
 
-        Button Scanner_btn = findViewById(R.id.Scanner_btn);
+        ImageButton Scanner_btn = findViewById(R.id.Scanner_btn);
         Scanner_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,7 +25,7 @@ public class hlogin extends AppCompatActivity {
                 startActivity(goTOScannerPage);
             }
         });
-        Button addhar_btn = findViewById(R.id.Adhhar_btn);
+        ImageButton addhar_btn = findViewById(R.id.Adhhar_btn);
         addhar_btn.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
